45dd3e830103426546306aca78ff6940d210cdf9
Your Name <you@example.com>
2021-04-27 15:00:18 +0530

    added editor component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import Editor from './Editor';
-import './App.css';
+import React, { useState } from "react";
 
+//Styles
+import "./index.css";
+
+//Components
+import Editor from "./Editor";
 
 function Upper() {
+  const [editorLanguage, setEditorLanguage] = useState("");
+
   return (
     <>
-    <div className="upper-pane">
-      <div class="container">
-        <div class="left"> 
-          <Editor/> 
-        </div>
-        <div class="right"> 
-          <Editor/> 
+      <div className="upper-pane">
+        <div class="container">
+          <div class="left">
+            <Editor
+              language="python"
+              displayName="Python"
+              value={editorLanguage}
+              onChange={setEditorLanguage}
+            />
+          </div>
+          <div class="right">
+            <div class="editor">
+              <p class="output"> Dummy Code </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="lower-pane">
-      <div class="editor">
-         <p> Dummy Code </p>
+      <div className="lower-pane">
+        <div class="editor">
+          <p class="output"> Dummy Code </p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
--- src/Editor.js
+++ src/Editor.js
@@ -1,12 +1,54 @@
-import React, {Component} from 'react';
+//necessary dependencies
+import React, { Component } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
-export default class Editor extends Component {
+// import codemirror Python mode
+import "codemirror/mode/python/python";
+// import codemirror C-like mode (includes C, C++ and Java)
+import "codemirror/mode/clike/clike";
 
-    render(){
-        return(
-            <div class="editor">
-                <p> Dummy Code </p>
-            </div>
-        )
-    }
-}
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { faLanguage } from "@fortawesome/free-solid-svg-icons";
+
+//functional component
+const Editor = (props) => {
+  const { language, displayName, value, onChange } = props;
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
+  return (
+    <div className="code-editor">
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          //theme-changing
+          theme: "material",
+        }}
+      />
+      <div className="selection"> {displayName} </div>
+    </div>
+  );
+};
+
+export default Editor;
+
+//// syntax for class based component
+// export default class Editor extends Component {
+
+//     render(){
+//         return(
+//             <div class="editor">
+//                 <p> Dummy Code </p>
+//             </div>
+//         )
+//     }
+// }
